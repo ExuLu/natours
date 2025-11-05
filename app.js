d7db007ca132968eb2665451ad9b5d541f925cd0
Alena Sharai <exultantislupus@gmail.com>
2025-11-05 20:23:21 +0000

    refactor: move tour and user routes to separate folders

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -12,164 +13,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// CONSTANTS
-const notFoundRes = {
-  status: 'fail',
-  message: 'Invalid id',
-};
-
-// I/O OPERATIONS
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-// UTILS
-const writeToursFile = (tours, responseCallback) =>
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    responseCallback
-  );
-const findTourIndex = (tourId) => tours.findIndex((tour) => tour.id === tourId);
-
-// ROUTE HANDLERS: TOURS
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    requestedAt: req.requestTime,
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTourById = (req, res) => {
-  const tourIndex = findTourIndex(Number(req.params.id));
-
-  if (tourIndex === -1) {
-    return res.status(404).json(notFoundRes);
-  }
-
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    data: {
-      tour: tours[tourIndex],
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours.at(-1).id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  const responseCallback = (err) => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  };
-  writeToursFile(tours, responseCallback);
-};
-
-const updateTour = (req, res) => {
-  const tourIndex = findTourIndex(Number(req.params.id));
-
-  if (tourIndex === -1) {
-    return res.status(404).json(notFoundRes);
-  }
-
-  const updatedTour = {
-    ...tours[tourIndex],
-    ...req.body,
-  };
-  const updatedTours = [...tours];
-  updatedTours[tourIndex] = updatedTour;
-
-  const responseCallback = (err) => {
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: updatedTour,
-      },
-    });
-  };
-  writeToursFile(updatedTours, responseCallback);
-};
-
-const deleteTour = (req, res) => {
-  const tourId = Number(req.params.id);
-  const tourIndex = findTourIndex(tourId);
-
-  if (tourIndex === -1) {
-    return res.status(404).json(notFoundRes);
-  }
-
-  const updatedTours = tours.filter((tour) => tour.id !== tourId);
-  const responseCallback = (err) => {
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  };
-  writeToursFile(updatedTours, responseCallback);
-};
-
-// ROUTE HANDLERS: USERS
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const getUserById = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-// ROUTES: TOURS
-const userRouter = express.Router();
-const tourRouter = express.Router();
-
+// ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
-
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
-
-// ROUTES: USERS
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser);
 
 // START SERVER
 const port = 3000;
---
+++ routes/tourRoutes.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+const notFoundRes = {
+  status: 'fail',
+  message: 'Invalid id',
+};
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+// UTILS
+const writeToursFile = (tours, responseCallback) =>
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    responseCallback
+  );
+const findTourIndex = (tourId) => tours.findIndex((tour) => tour.id === tourId);
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    requestedAt: req.requestTime,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTourById = (req, res) => {
+  const tourIndex = findTourIndex(Number(req.params.id));
+
+  if (tourIndex === -1) {
+    return res.status(404).json(notFoundRes);
+  }
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    data: {
+      tour: tours[tourIndex],
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours.at(-1).id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  const responseCallback = (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  };
+  writeToursFile(tours, responseCallback);
+};
+
+const updateTour = (req, res) => {
+  const tourIndex = findTourIndex(Number(req.params.id));
+
+  if (tourIndex === -1) {
+    return res.status(404).json(notFoundRes);
+  }
+
+  const updatedTour = {
+    ...tours[tourIndex],
+    ...req.body,
+  };
+  const updatedTours = [...tours];
+  updatedTours[tourIndex] = updatedTour;
+
+  const responseCallback = (err) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: updatedTour,
+      },
+    });
+  };
+  writeToursFile(updatedTours, responseCallback);
+};
+
+const deleteTour = (req, res) => {
+  const tourId = Number(req.params.id);
+  const tourIndex = findTourIndex(tourId);
+
+  if (tourIndex === -1) {
+    return res.status(404).json(notFoundRes);
+  }
+
+  const updatedTours = tours.filter((tour) => tour.id !== tourId);
+  const responseCallback = (err) => {
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  };
+  writeToursFile(updatedTours, responseCallback);
+};
+
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
+
+module.exports = router;
