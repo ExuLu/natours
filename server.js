94de707c597b445baa686143c6ce89170308f885
Alena Sharai <exultantislupus@gmail.com>
2025-11-07 14:34:40 +0000

    feat: create mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB);
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
