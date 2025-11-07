beafab225a4faba9aab2b215718e67046c2cbc97
Alena Sharai <exultantislupus@gmail.com>
2025-11-07 18:24:57 +0000

    feat: add error handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,14 +15,21 @@ exports.getTourById = (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-  const newTour = await Tour.create(req.body);
+  try {
+    const newTour = await Tour.create(req.body);
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
