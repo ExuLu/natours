aea9cdd4c956ffa9ed0c4050e3baf06071352b93
Alena Sharai <exultantislupus@gmail.com>
2025-11-07 15:05:38 +0000

    feat: create document and save it to db

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ const tourSchema = new mongoose.Schema({
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => console.log('ERROR 💣', err));
 
 const app = require('./app');
 
