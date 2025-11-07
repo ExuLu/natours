4741b856ef5213d69db105084df1d24e970bed24
Alena Sharai <exultantislupus@gmail.com>
2025-11-07 15:22:02 +0000

    chores: change tour data

--- server.js
+++ server.js
@@ -31,8 +31,7 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
+  name: 'The Park Camper',
   price: 497,
 });
 
