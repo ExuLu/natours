7ed6c49eab4462e9b8dc99f68cfdbd0df33bfca2
Alena Sharai <exultantislupus@gmail.com>
2025-11-07 21:12:46 +0000

    feat: create function to delete files

--- dev-data/data/importDevData.js
+++ dev-data/data/importDevData.js
@@ -26,6 +26,16 @@ const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
 const importData = async () => {
   try {
     await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany({});
+    console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
   }
