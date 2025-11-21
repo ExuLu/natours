a147ab810841cf27fd6b26090f2fcb7308288d65
Alena Sharai <exultantislupus@gmail.com>
2025-11-21 22:49:11 +0000

    feat: hashing and salting password before put it to db

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name, email, photo, password, passwordConfirm
 const userSchema = new mongoose.Schema({
@@ -32,10 +33,12 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', function (next) {
-  if (this.isModified('password')) return next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
 
-  
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
   next();
 });
 
