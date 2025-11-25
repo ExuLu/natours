950a41216a78eb02a99338284591fc237bc02f24
Alena Sharai <exultantislupus@gmail.com>
2025-11-25 17:43:44 +0000

    feat: check if user still exist

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,7 +61,9 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
 
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
+  const freshUser = await User.findById(decoded.id);
+
+  if (!freshUser) return next(new AppError('The token belonging to this user does not longer exist'));
 
   next();
 });
