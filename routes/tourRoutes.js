e6bfbbd47510ca4bbfb130cd5fef42b53fdbbfa6
Alena Sharai <exultantislupus@gmail.com>
2025-11-26 09:33:40 +0000

    chores: liter update

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,13 +7,22 @@ const router = express.Router();
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-router.route('/top-5-best').get(tourController.aliasTopTours, tourController.getAllTours);
+router
+  .route('/top-5-best')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.createTour);
+router
+  .route('/')
+  .get(authController.protect, tourController.getAllTours)
+  .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTourById)
   .patch(tourController.updateTour)
-  .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour,
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,14 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword', authController.resetPassword);
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
-router.route('/:id').get(userController.getUserById).patch(userController.updateUser).delete(userController.deleteUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUserById)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
