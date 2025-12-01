c6e2b7b15911c9994d2bdd2962298c91c8996bbf
Alena Sharai <exultantislupus@gmail.com>
2025-12-01 18:11:48 +0000

    chores: add comments

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARES
-
+// Set security HTTP headers
 app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -25,7 +26,10 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+// Body parser, reading data from body into req.body
 app.use(express.json());
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
