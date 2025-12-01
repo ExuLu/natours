76c841aa2d84da83ad0f52d18caca0e7b08ef4b3
Alena Sharai <exultantislupus@gmail.com>
2025-12-01 18:12:06 +0000

    feat: add limit to data

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
 // Test middleware
 app.use((req, res, next) => {
