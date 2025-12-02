cfa68eba794179bbe2e7db9ba74a90ea6cff3451
Alena Sharai <exultantislupus@gmail.com>
2025-12-02 20:11:05 +0000

    refactor: move populating to preFind hook

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,10 +22,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTourById = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate({
-    path: 'guides',
-    select: '-__v -passwordChangedAt',
-  });
+  const tour = await Tour.findById(req.params.id);
 
   if (!tour) {
     return next(new AppError('No tour found with this id', 404));
--- models/tourModel.js
+++ models/tourModel.js
@@ -111,6 +111,15 @@ tourSchema.pre('save', function (next) {
 });
 
 tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
 
