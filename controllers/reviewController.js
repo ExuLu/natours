8318df6425ceea64f81182e352206fd391d06c6f
Alena Sharai <exultantislupus@gmail.com>
2025-12-03 15:26:00 +0000

    feat: create review create handler

---
+++ controllers/reviewController.js
@@ -0,0 +1,13 @@
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
