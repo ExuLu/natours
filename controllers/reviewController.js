57ae7b583303c4323ca60a4bc24dd8fa5350e211
Alena Sharai <exultantislupus@gmail.com>
2025-12-03 15:45:19 +0000

    feat: create get reviews handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,13 +1,31 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create({ ...req.body, user: req.user._id });
+  const features = await Review.create({ ...req.body, user: req.user._id });
 
   res.status(201).json({
     status: 'success',
     data: {
       review: newReview,
+    },
+  });
+});
+
+exports.getReviews = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const reviews = await features.query;
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
     },
   });
 });
