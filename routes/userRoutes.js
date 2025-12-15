6290fb4b2858a2c6ba2a891132350fc9b8461cd2
Alena Sharai <exultantislupus@gmail.com>
2025-12-15 22:13:29 +0000

    feat: create delete user and add it to routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -26,6 +26,11 @@ router
   .route('/:id')
   .get(userController.getUserById)
   .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+  .delete(userController.deleteUser)
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.deleteUser,
+  );
 
 module.exports = router;
