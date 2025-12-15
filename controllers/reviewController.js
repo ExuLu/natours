46a947f0a2c3d5e53cd6a38bbdc462f2ae168383
Alena Sharai <exultantislupus@gmail.com>
2025-12-15 23:47:18 +0000

    refactor: extract middleware and handler from review create review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -38,22 +38,13 @@ exports.getReviewById = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOneHandler(Review);
+
+exports.setTourUserIds = catchAsync(async (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user._id;
 
-  const newReview = await Review.create({
-    ...req.body,
-  });
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    },
-  });
+  next();
 });
-
-exports.updateReview = factory.updateOne(Review);
-
-exports.deleteReview = factory.deleteOneHandler(Review);
