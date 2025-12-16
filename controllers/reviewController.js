ebb3775844a0cb3cdc9cf881bb2e1145f2681546
Alena Sharai <exultantislupus@gmail.com>
2025-12-16 15:35:29 +0000

    fix: delete unnecessary catchAsync

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,4 @@
 const Review = require('../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getReviews = factory.getAll(Review);
@@ -8,9 +7,9 @@ exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOneHandler(Review);
 
-exports.setTourUserIds = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user._id;
 
   next();
-});
+};
