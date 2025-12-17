65eda987a7ab8bbd1d288c6acc969abab4c995df
Alena Sharai <exultantislupus@gmail.com>
2025-12-17 20:24:52 +0000

    feat: calculate radians

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,6 +14,8 @@ exports.deleteTour = factory.deleteOneHandler(Tour);
 exports.getToursWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
   if (!lat || !lng) {
     return next(
       new AppError(
@@ -25,12 +27,13 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
 
   const tours = await Tour.find({
     startLocation: {
-      $geoWithin: { $center: [[lng, lat], distance] },
+      $geoWithin: { $centerSphere: [[lng, lat], radius] },
     },
   });
 
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       data: tours,
     },
