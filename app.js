b107585e4e3cc3f8eaeae99b578829e6fbfa425b
Alena Sharai <exultantislupus@gmail.com>
2025-12-19 16:15:00 +0000

    refactor: move routes to separate file

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -78,21 +79,7 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
+app.use('/', viewRouter);
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
---
+++ routes/viewRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
+router.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+router.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
+
+module.exports = router;
