5c2da0420bd50de2f975aac67c4531a9c17b5a95
Alena Sharai <exultantislupus@gmail.com>
2025-12-23 21:29:59 +0000

    feat: add signup functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
     role: req.body.role,
   });
--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,34 @@
 import '@babel/polyfill';
-import { login } from './login';
+import { login, signUp } from './authRequest';
 import { displayMap } from './leaflet';
 
 const map = document.getElementById('map');
-const form = document.querySelector('.form');
+const loginForm = document.getElementById('loginForm');
+const signUpForm = document.getElementById('signUpForm');
 
 if (map) {
   const locations = JSON.parse(map.dataset.locations);
   displayMap(locations);
 }
 
-if (form) {
-  form.addEventListener('submit', (e) => {
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
+  });
+}
+
+if (signUpForm) {
+  signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    signUp(name, email, password, confirmPassword);
   });
 }
