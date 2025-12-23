acb414dafd52242b97f33851980c42ba8bb61706
Alena Sharai <exultantislupus@gmail.com>
2025-12-23 22:31:40 +0000

    feat: create logout controller and add route

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,6 +61,15 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, res, 200, undefined);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
 
