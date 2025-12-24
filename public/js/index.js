3741448f0117757d37972a7c72c7065502424036
Alena Sharai <exultantislupus@gmail.com>
2025-12-24 20:33:47 +0000

    feat: add request to updateMe api endpoint

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 import '@babel/polyfill';
 import { login, signUp, logout } from './authRequest';
 import { displayMap } from './leaflet';
+import { updateUserData } from './updateUser';
 
 const map = document.getElementById('map');
 const loginForm = document.getElementById('loginForm');
 const signUpForm = document.getElementById('signUpForm');
 const logoutButton = document.querySelector('.nav__el--logout');
+const updateUserDataForm = document.querySelector('.form-user-data');
 
 if (map) {
   const locations = JSON.parse(map.dataset.locations);
@@ -31,6 +33,17 @@ if (signUpForm) {
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
     signUp(name, email, password, confirmPassword);
+  });
+}
+
+if (updateUserDataForm) {
+  updateUserDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    updateUserData(name, email);
   });
 }
 
