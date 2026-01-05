1a696118c98395b1ade6f68eb8865b8816f06a75
Alena Sharai <exultantislupus@gmail.com>
2026-01-05 13:24:56 +0000

    feat: update tour template

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,7 +2,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('../models/tourModel');
-const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
